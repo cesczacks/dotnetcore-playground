82f416069c44b9a197df2cd584dd29b7feaeb0fa
qinhong.wang <qinhong.wang@accenture.com>
2020-03-25 22:07:57 +0800

    add DI samples

---
+++ DotnetCorePlayground/Controllers/MessageController.cs
@@ -0,0 +1,35 @@
+﻿using DotnetCorePlayground.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCorePlayground.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class MessageController : ControllerBase
+	{
+
+		private readonly IMessage _messageService;
+
+		public MessageController(IMessage messageService)
+		{
+			_messageService = messageService;
+		}
+
+		[HttpGet("GetMessage")]
+		public string GetMessage()
+		{
+			return _messageService.PrintMessage();
+		}
+
+		[HttpGet("GetMessageCount")]
+		public int GetMessageCount() 
+		{
+			_messageService.MessageCountIncrement();
+			return _messageService.MessageCount;
+		}
+	}
+}
---
+++ DotnetCorePlayground/Services/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCorePlayground.Services
+{
+	public class Message : IMessage
+	{
+		public int MessageCount { get; set; }
+
+		public void MessageCountIncrement()
+		{
+			MessageCount++;
+		}
+
+		public string PrintMessage() => $"Hello, this is a message from {nameof(Message)}";
+		
+	}
+}
