e6800c66c4211fd2599dcfee892d8df45fe5edcd
Qinhong Wang <qinhong.wang@accenture.com>
2020-04-29 11:44:22 +0800

    move usings

--- DotnetCorePlaygroundTests/MessageControllerTests.cs
+++ DotnetCorePlaygroundTests/MessageControllerTests.cs
@@ -2,9 +2,6 @@
 using DotnetCorePlayground.Services;
 using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DotnetCorePlaygroundTests
 {
